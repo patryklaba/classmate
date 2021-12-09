79cbde2191629ecf2ab99f53dafcaf4efd2c6241
patryklaba <laba.patryk@gmail.com>
2021-12-09 12:42:51 +0100

    Basic styling added

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,25 @@
-import UsersList from '../components/UsersList/UsersList';
+import UsersList from '../components/organisms/UsersList/UsersList';
+import styled, { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/globalStyle';
+import { theme } from 'assets/styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 const Root = () => {
   return (
-    <>
-      <UsersList />
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Wrapper>
+        <UsersList />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
 
