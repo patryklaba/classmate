6691cc15b86fce4e05f20af5dcfeab7dd39633d3
patryklaba <laba.patryk@gmail.com>
2022-01-02 13:52:28 +0100

    NavLink styling

--- src/components/organisms/Navigation/Navigation.js
+++ src/components/organisms/Navigation/Navigation.js
@@ -14,8 +14,8 @@ const Navigation = () => {
       </Logo>
       <StyledLink to="/">Dashboard</StyledLink>
       <StyledLink to="/add-user">Add user</StyledLink>
-      <StyledLink to="/">Settings</StyledLink>
-      <StyledLink to="/">Logout</StyledLink>
+      {/* <StyledLink to="/">Settings</StyledLink>
+      <StyledLink to="/">Logout</StyledLink> */}
     </Wrapper>
   );
 };
